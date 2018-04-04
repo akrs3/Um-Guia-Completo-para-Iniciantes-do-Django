2e0526e07f55a94404e95ec139cde0391a384a3a
Anne Kelly <annekelly533@gmail.com>
2018-04-04 16:00:26 -0300

    Concluindo parte II - Tutorial

--- myproject/myproject/boards/tests.py
+++ myproject/myproject/boards/tests.py
@@ -1,3 +1,21 @@
+from django.core.urlresolvers import reverse
+from django.urls import resolve
 from django.test import TestCase
+from .views import home
 
-# Create your tests here.
+
+
+class HomeTests(TestCase):
+    
+    #Testando o codigo de status da resposta
+    #200 e o codigo de sucesso, se houvesse alguma excecao o Django retornaria um codigo 500
+    def test_home_view_status_code(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    #Testando se Django esta retornando a view correta na chamada do url
+    def test_home_url_resolves_home_view(self):
+        view = resolve('/') #garante q o url / que é o raiz retorne a view home
+        self.assertEquals(view.func, home)
+

896a2b072155644ffeeb72da6d8d1c05471408ae
Anne Kelly <annekelly533@gmail.com>
2018-04-13 16:04:47 -0300

    COnfigurando o.env e atualizando o requirements

--- myproject/myproject/myproject/settings.py
+++ myproject/myproject/myproject/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+from decouple import config
+from decouple import config, Csv
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +23,17 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$(&lvwcz7j+isg&9i2%#7xqz=p4ypx#wezd(_pn46g4fq^7o4='
+#SECRET_KEY = '$(&lvwcz7j+isg&9i2%#7xqz=p4ypx#wezd(_pn46g4fq^7o4='
+
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+DATABASES = {
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
+}
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -138,3 +151,4 @@ LOGIN_REDIRECT_URL = 'home'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 LOGIN_URL = 'login'
+
